ea4adeec50856189f3e60d78c816c19be8aa1a26
Stefan Hoffmann <stho32github@gmail.com>
2020-04-19 16:01:00 +0200

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Too much time?
+
+Here is your problem: You actually could do a lot of different things.
+Unfurtunately, since corona happened, the world has slowed down.
+
+Now you still have a lot of things that you should do, but there is
+a little kick that is missing. Someone or something that can propose
+the next best thing to do to you, whenever you loose your motivation.
+
+This powersell script reads the contents of the csv file and 
+proposes a random sequence of things to do. 
+
+You add a little time in minutes to each task, so this is the time you
+should at minimum use to do that whatever. 
+It will show a progress bar. 
+
+And because you do not like simple boring scripts, it will talk to you
+and even applaude you for having accomplished each of the random missions.
+
+Of cause, the list of things to do is just an example which can be modified
+and extended by you as you see fit.
---
+++ Trainer.ps1
@@ -0,0 +1,45 @@
+﻿Set-Location $PSScriptRoot
+Add-Type -AssemblyName System.speech
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoice('Microsoft David Desktop')
+
+$durationPerRoundInSeconds = 25*60
+#$durationPerRoundInSeconds = 5
+
+Get-Content .\things-to-do.csv | ConvertFrom-Csv | Get-Random -Count 5 | ForEach-Object {
+    $task = $_.name
+    $durationInMinutes = [int]$_.durationInMinutes
+    [decimal]$durationInSeconds = 60*$durationInMinutes
+    [decimal]$remainingSeconds = 60*$durationInMinutes
+
+    Clear-Host
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host "Proposal for the next task : $task" -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+
+    $speak.Speak("Proposal for the next task : $task")
+
+    While ($remainingSeconds -gt 0) {
+        Start-Sleep -Seconds 1
+        $minutesRemaining = [Math]::Round( [decimal]($remainingSeconds/60), 2)
+        Write-Progress -Activity "$task; $minutesRemaining minutes remaining" -PercentComplete $percentComplete
+        $percentComplete = [Math]::Round( ($durationInSeconds-$remainingSeconds) / $durationInSeconds * 100 )
+        $remainingSeconds -= 1
+    }
+
+    $speak.Speak("Well done. You have completed the mission, you may now continue to the next task.")
+
+    Read-Host "Task complete. Enter for the next one."
+}
+
+Write-Host "Round complete."
+
